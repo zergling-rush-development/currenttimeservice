1294324efc030fb3ed5d2381536a719aadd317ce
zergling-rush-development <zergling.rush.development@gmail.com>
2023-11-01 14:33:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+
+await app.RunAsync();
